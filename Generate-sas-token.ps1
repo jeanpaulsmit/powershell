404521ee6da36ce45227247b2f5670cbd7db0a8a
Jean-Paul Smit <github@didago.nl>
2017-10-31 17:40:13 +0100

    Script to download blob storage file

---
+++ Generate-sas-token.ps1
@@ -0,0 +1,25 @@
+﻿
+$AzureRmSubscriptionName = (Get-AzureRmContext).Subscription.SubscriptionName
+
+Select-AzureRmSubscription -SubscriptionName "IM-DEV (Enterprise)" #$AzureRmSubscriptionName
+
+$AzureResourceGroupName = "some-rg"
+$AzureStorageAccountName = "somesa"
+$AzureStorageContainerName = "somecontainer"
+
+$Keys = Get-AzureRmStorageAccountKey -ResourceGroupName $AzureResourceGroupName -StorageAccountName $AzureStorageAccountName
+$StorageAccountKey = $Keys[0].Value
+
+$storageContext = New-AzureStorageContext -StorageAccountName $AzureStorageAccountName -StorageAccountKey $StorageAccountKey
+
+$now=get-date
+$sastoken = New-AzureStorageContainerSASToken -Name $AzureStorageContainerName -Context $storageContext -Permission rl -StartTime $now.AddHours(-1) -ExpiryTime $now.AddMonths(1)
+
+$sastoken
+
+$storageuri = "https://$AzureStorageAccountName.blob.core.windows.net/$AzureStorageContainerName"
+$url = $storageUri + "/Import-export-storage-table-module.psm1" + $sastoken
+
+$output = "d:/tmp/Import-export-storage-table-module.psm1"
+
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
