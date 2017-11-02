2d12dcec30ce97549aeee08dc8ada89647dc2400
Jean-Paul Smit <github@didago.nl>
2017-11-02 10:40:02 +0100

    Script to be used in release definition to execute refresh of storage table

---
+++ Release-task-import-storage-table.ps1
@@ -0,0 +1,28 @@
+﻿# Arguments -> Output of "Create SAS Token" task
+# -_artifactsLocation "$(storageUriPowershellContainer)" -_artifactsLocationSasToken "$(storageTokenPowershellContainer)"
+
+
+# Powershell -> Inline for "Run Inline Azure Powershell" task
+Param([string]$_artifactsLocation , [string]$_artifactsLocationSasToken)
+
+###########################################################################
+# Variables needed to find the storage table to import into
+###########################################################################
+$AzureRmSubscriptionName = (Get-AzureRmContext).Subscription.SubscriptionName
+$AzureResourceGroupName = "rg-name"
+$AzureStorageAccountName = "saname"
+$AzureStorageTableName = "sa-table-name"
+$PathToCsv = "$(System.DefaultWorkingDirectory)/artifactlocation-from-build/storagetablecontent/test.csv"
+
+###########################################################################
+# Download the Powershell module containing the import function (must be downloaded as Import-Module is expecting the module on disk!)
+###########################################################################
+$PowershellModule = "Import-export-storage-table-module.psm1"
+$urlPowershellModule = "$_artifactsLocation/$PowershellModule" + "$_artifactsLocationSasToken"
+$dlpathPowershellModule = "$Env:SYSTEM_DEFAULTWORKINGDIRECTORY\Import-export-storage-table-module.psm1"
+(New-Object System.Net.WebClient).DownloadFile($urlPowershellModule, $dlpathPowershellModule)
+
+Import-Module -Name $dlpathPowershellModule
+
+Select-AzureRmSubscription -SubscriptionName $AzureRmSubscriptionName
+Import-AzureTableStorage -ResourceGroupName $AzureResourceGroupName -StorageAccountName $AzureStorageAccountName -TableName $AzureStorageTableName -Path $PathToCsv
