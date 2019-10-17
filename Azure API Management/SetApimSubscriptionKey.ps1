f4edf70d32ccec05e33c8aafafe6aee4f8ee5d73
Jean-Paul Smit <github@didago.nl>
2019-10-17 12:29:56 +0200

    Update SetApimSubscriptionKey.ps1

--- Azure API Management/SetApimSubscriptionKey.ps1
+++ Azure API Management/SetApimSubscriptionKey.ps1
@@ -1,25 +1,24 @@
+#--------------------------------------------------------------------------
+# Script to set a custom subscription key on APIM subscriptions
+# Used to align subscription keys across APIM regions (with standard tier)
+#--------------------------------------------------------------------------
 param
 (
     [string] $resourceGroupName,
     [string] $apimServiceName,
-    [string] $userEmail,
+    [string] $productId,
+    [string] $userId,
     [string] $subscriptionKey
 )
-
-# Install package provider to be able to install modules
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Scope CurrentUser -Force
 
 # Allow cmdlets for APIM to be used
 Install-Module -Name Az.ApiManagement -AllowClobber -Scope CurrentUser -force
 
 # Get reference to APIM instance
-$apimContext = New-AzureRmApiManagementContext -ResourceGroupName $resourceGroupName -ServiceName $apimServiceName
+$apimContext = New-AzApiManagementContext -ResourceGroupName $resourceGroupName -ServiceName $apimServiceName
 
-# Find the user Id based on the email
-$user = Get-AzureRmApiManagementUser -Context $apimContext -Email $userEmail
+# Retrieve the subscription to modify
+$subscription = Get-AzApiManagementSubscription -Context $apimContext -ProductId $productId -UserId $userId
 
-# Retrieve the subscriptions to modify
-$subscription = Get-AzureRmApiManagementSubscription -Context $apimContext -UserId $user.UserId
-
-# Set subscription key on the first subscription (probably will be only one)
-Set-AzureRmApiManagementSubscription -Context $apimContext -SubscriptionId $subscription[0].SubscriptionId -PrimaryKey $subscriptionKey
+# Set custom subscription key
+Set-AzApiManagementSubscription -Context $apimContext -SubscriptionId $subscription.SubscriptionId -PrimaryKey $subscriptionKey -SecondaryKey $subscription.SecondaryKey -State $subscription.State
