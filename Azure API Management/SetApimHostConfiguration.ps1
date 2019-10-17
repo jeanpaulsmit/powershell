ede20ee9a8cae16d877e14bc8e3584bb58002b5c
Jean-Paul Smit <github@didago.nl>
2019-10-17 12:30:29 +0200

    Update SetApimHostConfiguration.ps1

--- Azure API Management/SetApimHostConfiguration.ps1
+++ Azure API Management/SetApimHostConfiguration.ps1
@@ -1,21 +1,19 @@
+#--------------------------------------------------------------
+# Script to setup custom proxy host configuration on APIM
+#--------------------------------------------------------------
 param
 (
     [string] $resourceGroupName,
     [string] $apimServiceName,
     [string] $apiProxyHostname,
-    [string] $pfxCertificateLocation,
-    [string] $pfxCertificatePwd
+    [string] $kvCertificateSecret
 )
 
-# Install package provider to be able to install modules
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Scope CurrentUser -Force
-
-# Allow cmdlets for APIM to be used
+# Allow cmdlets to be used
 Install-Module -Name Az.ApiManagement -AllowClobber -Scope CurrentUser -force
 
 # Create the HostnameConfiguration object for Proxy endpoint
-$securePassword = ConvertTo-SecureString -String $pfxCertificatePwd -AsPlainText -Force
-$proxyConfiguration = New-AzApiManagementCustomHostnameConfiguration -Hostname $apiProxyHostname -HostnameType Proxy -PfxPath $pfxCertificateLocation -PfxPassword $securePassword
+$proxyConfiguration = New-AzApiManagementCustomHostnameConfiguration -Hostname $apiProxyHostname -HostnameType Proxy -KeyVaultId $kvCertificateSecret
 
 # Get reference to APIM instance and apply the configuration to API Management
 $apimContext = Get-AzApiManagement -ResourceGroupName $resourceGroupName -Name $apimServiceName
